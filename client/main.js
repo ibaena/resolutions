14e74958e9bb048c41ce79310e15d54341fb7a65
Ivan <ivan.baena04@gmail.com>
2016-04-28 04:20:14 -0400

    set up mongodb connection

--- client/main.js
+++ client/main.js
@@ -1,22 +1,18 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
+//IMPORTS
+import {
+  Template
+} from 'meteor/templating';
+import {
+  ReactiveVar
+} from 'meteor/reactive-var';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+//CREATES MONGO COLLECTION resolutions
+Resolutions = new Mongo.Collection('resolutions');
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.body.helpers({
+  resolutions: function(){
+    console.log(Resolutions.find({}));
+    return Resolutions.find({});
+  }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+//MONGO CONNECTION
+
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Resolutions = new Mongo.Collection('resolutions');
 });
