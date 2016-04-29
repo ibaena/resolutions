d9ecf34cb0a2a3105fa0692553056c0b18f96944
Ivan <ivan.baena04@gmail.com>
2016-04-29 01:48:29 -0400

    attempting push to heroku

--- client/main.js
+++ client/main.js
@@ -43,22 +43,7 @@ Template.body.events({
     Session.set('hideFinished', event.target.checked);
   }
 });
-Template.resolution.helpers({
-  isOwner: function(){
-    return this.owner === Meteor.userId();
-  }
-});
-Template.resolution.events({
-  "click .delete": function() {
-    Meteor.call("deleteResolution", this._id);
-  },
-  "click .toggle-checked": function() {
-    Meteor.call("updateResolution", this._id, !this.checked);
-  },
-  "click .toggle-private": function(){
-    Meteor.call("setPrivate", this._id, !this.private);
-  }
-});
+
 
 //ACCOUNTS
 Accounts.ui.config({
